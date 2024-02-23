479a4259983c7b2b78672dc197a29c715dad2007
Albin <Albin.Svensson@medieinstitutet.se>
2024-02-23 10:40:23 +0100

    Styling

--- headsup/src/App.tsx
+++ headsup/src/App.tsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import "./App.css";
+import CardContainer from "./component/CardContainer";
 
 function App() {
-  return;
+  const [changeCard, setChangeCard] = useState(0);
+
+  function ChangeCardHandlerAdd() {
+    return setChangeCard(changeCard + 1);
+  }
+  function ChangeCardHandlerDecrease() {
+    return setChangeCard(changeCard - 1);
+  }
+  return (
+    <CardContainer
+      ChangeCardHandlerAdd={ChangeCardHandlerAdd}
+      ChangeCardHandlerDecrease={ChangeCardHandlerDecrease}
+      changeCard={changeCard}
+    />
+  );
 }
 
 export default App;
